f3b6c9cd6b9ccf21ee08201933e0394b8fda05a2
tanish2k9 <tanishchauhanmin@gmail.com>
2024-09-02 15:20:41 +0530

    initial commit

---
+++ src/main/java/com/tanish/jwtAuthenticationProject/JwtAuthenticationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tanish.jwtAuthenticationProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthenticationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtAuthenticationProjectApplication.class, args);
+	}
+
+}
